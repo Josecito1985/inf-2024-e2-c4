9cb3000d684411e190c0c7c1da96e2067f48c6b1
Jose Gomez <josepilar3@gmail.com>
2024-09-26 18:51:14 -0300

    agregamos nuevo commit

---
+++ numeropar.py
@@ -0,0 +1,8 @@
+# Solicitar un número al usuario
+numero = int(input("Ingresa un número: "))
+
+# Verificar si es par o impar
+if numero % 2 == 0:
+    print(f"El número {numero} es par.")
+else:
+    print(f"El número {numero} es impar.")
